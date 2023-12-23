aba2d73edfec9a1bfe642c5dfc10055770a54df7
charliejp21 <charliejp21@gmail.com>
2023-12-23 17:57:09 -0600

    formulario de presupuesto finalizado

---
+++ src/components/NuevoPresupuesto/NuevoPresupuestoStyles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+    label: {
+      textAlign: 'center',
+      fontSize: 24,
+      color: '#3B82F6'
+
+    },
+    input: {
+      backgroundColor: '#F5F5F5',
+      padding: 10,
+      borderRadius:10,
+      textAlign: 'center',
+      marginTop: 30
+
+    },
+    boton: { 
+      marginTop: 30,
+      backgroundColor: '#1048A4',
+      padding: 10,
+      borderRadius: 10
+
+    },
+    textoBoton: {
+      color: '#FFF',
+      textAlign: 'center',
+      textTransform: 'uppercase',
+      fontWeight: 'bold'
+    }
+    
+
+})
